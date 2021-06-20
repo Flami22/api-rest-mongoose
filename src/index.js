545fdf41a24c5b5f3e32e52024778dab0c1c93a1
João Henrique <joao.henrique-1998@hotmail.com>
2021-06-19 22:07:35 -0300

    Create of server and route configuration

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res) => {
-res.send('ok');
-});
-
 require('./controllers/authController')(app);
 
 app.listen(3000);
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
 name: {
@@ -25,6 +26,13 @@ createdAt: {
     default: Date.now,
 },
 });
+
+UserSchema.pre('save', async function(next) {
+        const hash = await bcrypt.hash(this.password, 10);
+        this.password = hash;
+        next();
+});
+
 
 const User = mongoose.model('User', UserSchema)
 
